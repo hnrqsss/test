8b50a8f2516fd201bce8eec2eacf6fb91c79ef54
hnrqsss <hnrq.assuncao@gmail.com>
2018-09-07 16:11:28 -0300

    set nestedStates

--- src/App.js
+++ src/App.js
@@ -1,6 +1,8 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
+import { Map, fromJS } from 'immutable'
 import './App.css';
+import update from 'immutability-helper'
 
 class App extends Component {
   
@@ -16,16 +18,27 @@ class App extends Component {
   
 
   handlerInput = (element) => {
-    this.setState({
-      [element.target.name] : element.target.value
-    })
+
+    const {name, value} = element.target
+   
+    const nestedArray = name.split('.');
+
+    let user = {...this.state.user}
+    
+    user = fromJS(user)
+    user = user.setIn(nestedArray , value);      
+    user = user.toJS()
+    
+    this.setState({user})
+    
   }
 
   render() {
     return (
       <div className="App">
-        {console.log(this.state)}
-        <input name='user.address.street' placeholder='address' onInput={this.handlerInput} />     
+        <input name='name' placeholder='Name' onInput={this.handlerInput} />
+        <input name='age' placeholder='age' onInput={this.handlerInput} />
+        <input name='address.street' placeholder='address' onInput={this.handlerInput} />
       </div>
     );
   }
